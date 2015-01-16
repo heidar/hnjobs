020b17d0dbe422708d81226a905a118e0309fab7
Heidar Bernhardsson <heidarbn@gmail.com>
2015-01-16 23:53:45 +0800

    clean up analyzer

--- analyzer.rb
+++ analyzer.rb
@@ -2,37 +2,17 @@
 
 require 'json'
 
-languages = [
-  'c',
-  'css',
-  'c++',
-  'c#',
-  'clojure',
-  'coffeescript',
-  'erlang',
-  'go',
-  'haskell',
-  'html',
-  'java',
-  'javascript',
-  'lua',
-  'objective-c',
-  'perl',
-  'php',
-  'python',
-  'ruby',
-  'scala'
-]
+languages = %w(c css c++ c# clojure coffeescript erlang go haskell html java 
+               javascript lua objective-c perl php python ruby scala)
 
-search = languages
-posts = JSON.parse(IO.read(ARGV[0]))
+posts = JSON.parse IO.read(ARGV[0])
 results = {}
 
 posts.each do |month, items|
-  results[month] = Hash[search.map {|key| [key, 0]}]
+  results[month] = Hash[languages.map {|key| [key, 0]}]
   items.each do |item, comments|
     comments.each do |comment|
-      words = comment.gsub(/[.,0-9()\/]/i, ' ').split()
+      words = comment.gsub(/[.,0-9()\/]/i, ' ').split
       words.each do |word|
         results[month][word.downcase] += 1 if languages.include?(word.downcase)
       end
@@ -41,3 +21,4 @@ posts.each do |month, items|
 end
 
 puts JSON.pretty_generate(results)
+
